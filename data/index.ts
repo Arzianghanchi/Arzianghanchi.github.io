5ab0c5f54e4e0244448275be524f622f5db01ac0
Azzy-Dev <118063625+Arzianghanchi@users.noreply.github.com>
2024-06-24 16:51:21 +0530

    Add files via upload

---
+++ data/index.ts
@@ -0,0 +1,131 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "We leverage a collaborative DevOps approach to streamline development and operations. ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/project1.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My Tech Stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a DevOps",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently Studying Devops Technologies",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Resume",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Automated Instance Restart using SSM Documents and Run Command",
+    des: "SSM document for restarting all EC2 instances:",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "www.google.com",
+  },
+  {
+    id: 2,
+    title: "Deployment of a Website on AWS using Appache Server",
+    des: "Deployment of a Website on AWS EC2 using Appache Server",
+    img: "/nike.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+ 
+
+];
+
+
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "AWS Cloud Intern Intern",
+    desc: "Created Cloud INfrastructure.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Devops Engineer",
+    desc: "Designed and developed CI/CD Pipeline",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+
+
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link:"https://github.com/Arzianghanchi"
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link:""
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link:"https://www.linkedin.com/in/arzian-ghanchi/"
+    
+  },
+];
