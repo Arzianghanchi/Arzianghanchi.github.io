90db8041f13eb59f74736a9af1c1948011bd6694
Azzy-Dev <118063625+Arzianghanchi@users.noreply.github.com>
2024-07-02 13:36:45 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,8 +17,7 @@ const Footer = () => {
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          Ready to take <span className="text-purple">your</span> digital
-          presence to the next level?
+           with a strong skillset in <span className="text-purple">Devops</span>. I'm eager to take on new challenges and deliver exceptional results.
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Reach out to me today and let&apos;s discuss how I can help you
